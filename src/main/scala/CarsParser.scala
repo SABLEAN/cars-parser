8dbd20a6c576728b1cfcf7e2639998e961e3836d
SABLEAN <michael.tkachenko1@mail.ru>
2021-11-16 18:15:53 +0600

    add defs and class

--- src/main/scala/CarsParser.scala
+++ src/main/scala/CarsParser.scala
@@ -1,8 +1,25 @@
+import cats.effect.{IO, Resource}
+
+import java.io.{FileInputStream, FileOutputStream}
+
 object CarsParser extends App {
 
-  case class Cars(year: Int, car: String)
+  case class Cars(year: Int, brand: String, model: String)
+  case class Cars2(car: String, years: List[Int])
 
-  val b: String = RiderFile.openInputStream("cars.csv")
+  def putAndDrop(fileNameIn: String, fileNameOut: String): Resource[IO, (FileInputStream, FileOutputStream)] =
+    for {
+      in  <- RiderFile.openInputStream(fileNameIn)
+      out <- RiderFile.openOutputStream(fileNameOut)
+
+    } yield (in, out)
+  def inputStreamDef(ff: FileInputStream): List[String] = ???
+  def parser(l: List[String]):List[Cars] = ???
+  def calculiator(l:List[Cars]):List[Cars2] = ???
+  def outputStreamDef(oo:FileOutputStream, l:List[Cars2]):Unit = ???
+
+  putAndDrop("cars.csv", "cars2.csv").use(streams => streams())
   println("Scala-parser test!!!")
-
+  val carsOut: Cars = RiderFile.openInputStream("cars.csv")
+  println("Scala-parser test!!!")
 }
--- src/main/scala/RiderFile.scala
+++ src/main/scala/RiderFile.scala
@@ -5,7 +5,7 @@ import java.io.{File, FileInputStream, FileOutputStream}
 object RiderFile {
 
   def openInputStream(fileName: String): Resource[IO, FileInputStream] =
-    Resource.make(getInputStreamF(fileName))(s => IO.delay(s.close()))
+    Resource.make(getInputStreamF(fileName))(s => IO(s.close()))
 
   def getInputStreamF(fileName: String): IO[FileInputStream] =
     IO {
@@ -13,14 +13,13 @@ object RiderFile {
     }
 
   def openOutputStream(fileName: String): Resource[IO, FileOutputStream] =
-    Resource.make(getOutputStreamF(fileName))(s => IO.delay(s.close()))
+    Resource.make(getOutputStreamF(fileName))(s => IO(s.close()))
 
   def getOutputStreamF(fileName: String): IO[FileOutputStream] =
     IO {
       new FileOutputStream(new File(fileName))
     }
 
-  def fileNames(): Set[String] =
-    Range.inclusive(1992, 2022).toSet.map[String](year => s"car-models-data/$year.csv")
+
 
 }
