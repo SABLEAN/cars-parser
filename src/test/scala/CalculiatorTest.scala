b3661b5247a102fee33c55d7da77849960b5de31
SABLEAN <michael.tkachenko1@mail.ru>
2021-11-22 18:04:55 +0600

    ADD new CalculatorTest

--- src/test/scala/CalculiatorTest.scala
+++ src/test/scala/CalculiatorTest.scala
@@ -2,10 +2,18 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class CalculiatorTest extends AnyWordSpec with Matchers {
+
   "Calculiator" should {
-    "back List[YearOfRelease]" in {
+    "back List[YearOfRelease]OneCar" in {
 
       Calculiator.calculiator(List(Car(1990, "BMW", "M3"))) shouldEqual List(YearOfRelease("BMW M3", List(1990)))
     }
+
+    "back List[YearOfRelease]MoreCar" in {
+
+      Calculiator.calculiator(List(Car(1990, "BMW", "M3"),Car(1990, "BMW", "M5"),Car(1991, "BMW", "M3"))) shouldEqual List(YearOfRelease("BMW M5",List(1990)), YearOfRelease("BMW M3",List(1990, 1991)))
+    }
+
   }
+
 }
