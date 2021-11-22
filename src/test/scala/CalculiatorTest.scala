7316e779230a2a5e2606608b12d0e720e6562f98
SABLEAN <michael.tkachenko1@mail.ru>
2021-11-22 18:45:35 +0600

    ADD new UnparserTest

--- src/test/scala/CalculiatorTest.scala
+++ src/test/scala/CalculiatorTest.scala
@@ -9,7 +9,7 @@ class CalculiatorTest extends AnyWordSpec with Matchers {
       Calculiator.calculiator(List(Car(1990, "BMW", "M3"))) shouldEqual List(YearOfRelease("BMW M3", List(1990)))
     }
 
-    "back List[YearOfRelease]MoreCar" in {
+    "back List[YearOfRelease]ManyCar" in {
 
       Calculiator.calculiator(List(Car(1990, "BMW", "M3"), Car(1990, "BMW", "M5"), Car(1991, "BMW", "M3"))) shouldEqual List(
         YearOfRelease("BMW M5", List(1990)),
--- src/test/scala/UnparserTest.scala
+++ src/test/scala/UnparserTest.scala
@@ -3,9 +3,22 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class UnparserTest extends AnyWordSpec with Matchers {
   "Parser" should {
-    "back List[String]" in {
+    "back List[String]OneYear" in {
 
       Unparser.unparser (List(YearOfRelease("BMW M3",List(1990)))) shouldEqual List("Car,Year", "BMW M3,[\"1990\"]")
     }
+
+    "back List[String]ManyYear" in {
+
+      Unparser.unparser (List(YearOfRelease("BMW M3", List(1990, 1992, 2000)))) shouldEqual List("Car,Year","BMW M3,[\"1990\",\"1992\",\"2000\"]")
+    }
+
+
   }
+
 }
+
+
+
+
+
