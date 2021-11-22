fd08b496837f8964a24cf7a4f4bf78081672b9f1
SABLEAN <michael.tkachenko1@mail.ru>
2021-11-22 18:45:46 +0600

    ADD new UnparserTest

--- src/test/scala/UnparserTest.scala
+++ src/test/scala/UnparserTest.scala
@@ -5,20 +5,14 @@ class UnparserTest extends AnyWordSpec with Matchers {
   "Parser" should {
     "back List[String]OneYear" in {
 
-      Unparser.unparser (List(YearOfRelease("BMW M3",List(1990)))) shouldEqual List("Car,Year", "BMW M3,[\"1990\"]")
+      Unparser.unparser(List(YearOfRelease("BMW M3", List(1990)))) shouldEqual List("Car,Year", "BMW M3,[\"1990\"]")
     }
 
     "back List[String]ManyYear" in {
 
-      Unparser.unparser (List(YearOfRelease("BMW M3", List(1990, 1992, 2000)))) shouldEqual List("Car,Year","BMW M3,[\"1990\",\"1992\",\"2000\"]")
+      Unparser.unparser(List(YearOfRelease("BMW M3", List(1990, 1992, 2000)))) shouldEqual List("Car,Year", "BMW M3,[\"1990\",\"1992\",\"2000\"]")
     }
-
 
   }
 
 }
-
-
-
-
-
