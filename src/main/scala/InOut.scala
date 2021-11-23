36d229064fcc7a0dc9ee8436eb33b110948a5389
SABLEAN <58905946+SABLEAN@users.noreply.github.com>
2021-11-23 21:04:51 +0600

    Add exeption for in\out file (#3)

--- src/main/scala/InOut.scala
+++ src/main/scala/InOut.scala
@@ -1,3 +1,6 @@
+import java.io.File
+import scala.runtime.Nothing$
+
 object InOut {
 
   def inOut(inOutFileName: List[String]): InOutFileName = {
@@ -5,8 +8,27 @@ object InOut {
     val in  = inOutFileName.find(b => b.startsWith("in="))
     val out = inOutFileName.find(b => b.startsWith("out="))
 
-    val inOutFileNameTrue: InOutFileName = InOutFileName(in.get.stripPrefix("in="), out.get.stripPrefix("out="))
-    inOutFileNameTrue
+    if (in.isDefined && out.isDefined) {
+      val inOutFileNameTrue: InOutFileName = InOutFileName(in.get.stripPrefix("in="), out.get.stripPrefix("out="))
+      val f: File                          = new File(inOutFileNameTrue.inFileName)
+      if (f.exists() && !f.isDirectory) {
+        inOutFileNameTrue
+      } else {
+        println("Error!!!")
+        println("Pls enter correct INFILENAME.csv:")
+        System.exit(-1)
+        ???
+
+      }
+
+    } else {
+      println("Error!!!")
+      println("Pls enter in/out file name for next format:")
+      println("in=INFILENAME.csv out=OUTFILENAME.csv")
+      System.exit(-1)
+      ???
+    }
+
   }
 
 }
